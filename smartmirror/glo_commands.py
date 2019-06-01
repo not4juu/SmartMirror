7afa9a42d67cb2891b56c5bed23328c642b2822b
not4juu <Sebastian.Kaluzny@fis.agh.edu.pl>
2019-06-02 01:32:50 +0200

    Applies command recognition handlers for news and weather

--- smartmirror/glo_commands.py
+++ smartmirror/glo_commands.py
@@ -1,8 +1,10 @@
 from smartmirror.glo_messages import GLO_MSG
 
 GLO_CMD = {
-    GLO_MSG['DISPLAY_WEATHER']: 'pogoda',
-    GLO_MSG['DISPLAY_DATE']: 'data',
+    GLO_MSG['DISPLAY_WEATHER']: 'pokaż pogodę',
+    GLO_MSG['HIDE_WEATHER']: 'ukryj pogodę',
+    GLO_MSG['DISPLAY_NEWS']: 'pokaż wiadomości',
+    GLO_MSG['HIDE_NEWS']: 'ukryj wiadomości',
     GLO_MSG['DISPLAY_CLOCK']: 'pokaż godzinę',
     GLO_MSG['HIDE_CLOCK']: 'ukryj godzinę',
 }
