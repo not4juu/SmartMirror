5041e5538e288fc78c5279c6b6c4beeeb7af742b
not4juu <Sebastian.Kaluzny@fis.agh.edu.pl>
2019-06-12 00:59:57 +0200

    Pulse text modification, stop , start animation set

--- smartmirror/window/pulse_text.py
+++ smartmirror/window/pulse_text.py
@@ -13,7 +13,6 @@ class PulseText:
         height = int(self.canvas.cget("height")) / 2
         self.text = self.canvas.create_text(width, height, fill=ApiSettings.Foreground,
                                             font=(ApiSettings.Font, ApiSettings.LargeTextSize), text="no text")
-        self.canvas.pack()
 
         self.run = False
         self.color = 0
@@ -37,21 +36,29 @@ class PulseText:
         self.canvas.itemconfig(self.text, fill=pulse_color)
 
         if self.run:
-            self.canvas.after(30, self._animation)
+            self.canvas.after(10, self._animation)
 
     def set_text(self, text):
         self.canvas.itemconfig(self.text, text=text)
         Logger.logging.debug("Set pulse text animation: {0}".format(text))
 
     def start_animation(self):
+        self.canvas.pack()
         self.run = True
         self._animation()
         Logger.logging.debug("Start pulse text animation")
 
     def stop_animation(self):
         self.run = False
+        self.canvas.pack_forget()
         Logger.logging.debug("Stop pulse text animation")
 
 
 if __name__ == "__main__":
-    pass
+    tk = Tk()
+    center_frame = Frame(tk, background=ApiSettings.Background)
+    center_frame.pack(side=TOP, fill=BOTH, expand=YES)
+    pulse = PulseText(center_frame)
+    pulse.set_text("Authorization")
+    pulse.start_animation()
+    tk.mainloop()
