aef09a3eeaec8d8ac3dcd38cb2604aa60ed17c3c
not4juu <Sebastian.Kaluzny@fis.agh.edu.pl>
2019-06-13 22:48:26 +0200

    New way of Logger working

--- smartmirror/Logger.py
+++ smartmirror/Logger.py
@@ -1,4 +1,4 @@
-import logging as syslogger
+import logging as sys_logging
 import sys
 
 """
@@ -16,21 +16,21 @@ def init_logger(logs_to_file=True, verbose=False):
         logging.warning("Logger instance exist!")
         return -1
 
-    logging = syslogger.getLogger(name="SmartMirror_Logger")
-    logging.setLevel(level=syslogger.DEBUG)
-    logger_formatter = syslogger.Formatter(
+    logging = sys_logging.getLogger(name="smartmirror.logger")
+    logging.setLevel(level=sys_logging.DEBUG)
+    logger_formatter = sys_logging.Formatter(
         '[%(asctime)s:%(threadName)s:%(module)s:%(lineno)s:%(levelname)s] %(message)s')
 
-    logger_console_handler = syslogger.StreamHandler(sys.stdout)
+    logger_console_handler = sys_logging.StreamHandler(sys.stdout)
     logger_console_handler.setLevel(
-        level=syslogger.DEBUG if verbose else syslogger.INFO)
+        level=sys_logging.DEBUG if verbose else sys_logging.INFO)
 
     logger_console_handler.setFormatter(logger_formatter)
     logging.addHandler(logger_console_handler)
 
     if logs_to_file:
-        logger_file_handler = syslogger.FileHandler(filename="smartmirror_logger.log", mode="w", encoding='utf-8')
-        logger_file_handler.setLevel(level=syslogger.DEBUG)
+        logger_file_handler = sys_logging.FileHandler(filename="smartmirror_logger.log", mode="w", encoding='utf-8')
+        logger_file_handler.setLevel(level=sys_logging.DEBUG)
         logger_file_handler.setFormatter(logger_formatter)
         logging.addHandler(logger_file_handler)
 
@@ -39,6 +39,9 @@ def init_logger(logs_to_file=True, verbose=False):
         logs_to_file, verbose))
 
     return 0
+
+
+Logger = sys_logging.getLogger(name="smartmirror.logger")  # Define Global Logger variable
 
 
 if __name__ == "__main__":
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,14 +1,17 @@
 import unittest
-from smartmirror import Logger
+from smartmirror.Logger import Logger, init_logger
 
 
 class TestLogger(unittest.TestCase):
 
     def test_init_logger(self):
-        self.assertEqual(Logger.init_logger(outIntoFile=False), 0)
+        self.assertEqual(init_logger(logs_to_file=False), 0)
 
     def test_init_logger_one_instance(self):
-        self.assertEqual(Logger.init_logger(outIntoFile=False), -1)
+        self.assertEqual(init_logger(logs_to_file=False), -1)
+
+    def test_info_print(self):
+        self.assertEqual(Logger.info("Test"), None)
 
 
 if __name__ == '__main__':
